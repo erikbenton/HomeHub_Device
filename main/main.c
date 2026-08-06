d166bda6d9b3b1e1cf8dfdd22e2dccd0c58e480c
Erik Benton <erik.benton@hotmail.com>
2026-08-06 15:20:56 -0700

    implementing basic wifi connect config menuconfig setup

--- main/main.c
+++ main/main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include "nvs_flash.h"
+#include "wifi_connect.h"
+#include "mqtt_client.h"
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
 void app_main(void)
 {
+    nvs_flash_init();
 
+    wifi_connect_init();
+
+    // Configure the Wifi SSID and password in the menuconfig
+    ESP_ERROR_CHECK(wifi_connect_sta(CONFIG_WIFI_SSID, CONFIG_WIFI_PASSWORD, 10000));
 }
