09a51163084531002270dad48cabbfe73918ecf9
Erik Benton <erik.benton@hotmail.com>
2026-08-07 15:45:57 -0700

    init implementation of MQTT broker communication for temperature devices

--- main/main.c
+++ main/main.c
@@ -5,10 +5,38 @@
 #include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "cJSON.h"
 
-static esp_mqtt_client_handle_t client;
+esp_mqtt_client_handle_t mqtt_device_client;
 
 static const char *TAG = "MQTT";
+char commands_topic[50];
+char temperature_state_topic[75];
+char temperature_value_topic[75];
+
+void mqtt_topic_controller(esp_mqtt_event_handle_t event)
+{
+    cJSON *payload;
+    if (strcmp(event->topic, temperature_state_topic) == 0)
+    {
+        ESP_LOGI(TAG, "temperature state endpoint hit");
+        // temperature state
+        payload = cJSON_ParseWithLength(event->data, event->data_len);
+        if (payload != NULL)
+        {
+            // state: for now will be boolean for on/off, can make enum later
+            cJSON *temp_state_json = cJSON_GetObjectItem(payload, "state");
+            bool is_on = cJSON_IsTrue(temp_state_json);
+            ESP_LOGI(TAG, "Device state: %s", is_on ? "on" : "off");
+        }
+        cJSON_Delete(payload);
+    }
+    else if (strcmp(event->topic, temperature_value_topic) == 0)
+    {
+        // temperature value
+        ESP_LOGI(TAG, "temperature value endpoint hit");
+    }
+}
 
 void mqtt_any_event_handler(void *event_handler_arg,
                             esp_event_base_t event_base,
@@ -26,6 +54,9 @@ void mqtt_any_event_handler(void *event_handler_arg,
         break;
     case MQTT_EVENT_CONNECTED:
         ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
+
+        // subscribe to necessary topics
+        esp_mqtt_client_subscribe(mqtt_device_client, commands_topic, 1);
         break;
     case MQTT_EVENT_DISCONNECTED:
         ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
@@ -43,6 +74,7 @@ void mqtt_any_event_handler(void *event_handler_arg,
         ESP_LOGI(TAG, "MQTT_EVENT_DATA");
         printf("topic: %.*s\n", event->topic_len, event->topic);
         printf("message: %.*s\n", event->data_len, event->data);
+        mqtt_topic_controller(event);
         break;
     case MQTT_EVENT_BEFORE_CONNECT:
         ESP_LOGI(TAG, "MQTT_EVENT_BEFORE_CONNECT");
@@ -57,6 +89,13 @@ void mqtt_any_event_handler(void *event_handler_arg,
     default:
         break;
     }
+}
+
+void init_dynamic_mqtt_topics(void)
+{
+    sprintf(commands_topic, "HomeHelper/hub/commands/%d/#", CONFIG_MQTT_DEVICE_ID);
+    sprintf(temperature_state_topic, "HomeHelper/hub/commands/%d/temperature/state", CONFIG_MQTT_DEVICE_ID);
+    sprintf(temperature_value_topic, "HomeHelper/hub/commands/%d/temperature/value", CONFIG_MQTT_DEVICE_ID);
 }
 
 void app_main(void)
@@ -76,8 +115,11 @@ void app_main(void)
             .msg = "ESP32 died =(",
             .msg_len = strlen("ESP32 died =(")}};
 
-    client = esp_mqtt_client_init(&esp_mqtt_client_config);
+    // initialize dynamic topics
+    init_dynamic_mqtt_topics();
 
-    esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_any_event_handler, NULL);
-    esp_mqtt_client_start(client);
+    mqtt_device_client = esp_mqtt_client_init(&esp_mqtt_client_config);
+
+    esp_mqtt_client_register_event(mqtt_device_client, ESP_EVENT_ANY_ID, mqtt_any_event_handler, NULL);
+    esp_mqtt_client_start(mqtt_device_client);
 }
